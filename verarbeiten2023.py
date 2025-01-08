2564bb6fe8200b8228a2ac026aee44094f0c7a18
thilob <thilob@gmx.net>
2025-01-08 20:49:20 +0100

    Code bereinigt, "nur Oberberg" eingebaut.

--- verarbeiten2023.py
+++ verarbeiten2023.py
@@ -1,40 +1,38 @@
 """Importe
 """
 import re
-import sys
 import requests
 import slugify
-import psycopg2 
-import zipfile 
+import psycopg2
 import shutil
 import psycopg2.extras
 from PySide6 import QtWidgets
 from DatenDialog_ui import Ui_datenDialog
 
 """Initialer Aufbau des Datenbank-Verbindung
+Der Cursor wird anschließend im weiteren Programm verwendet.
 """
 print("Programm gestartet")
-conn = psycopg2.connect("dbname=cebiusdaten user=cebiusdaten password=cebiusdaten")
+conn = psycopg2.connect(
+    "dbname=cebiusdaten user=cebiusdaten password=cebiusdaten")
 cur = conn.cursor()
 print("Verbindung hergestellt")
 
 
-
 def GebrefLadenUndEntpacken(url):
     """falls aktiviert, läd die Funktion die gezippte Gebäudereferenzdatei herunter und entpackt sie
-
     Args:
         url (URL-String): Die zum Download verwendete URL. Kann in der GUI angepasst werden.
     """
-    print ("Gebäudereferenzen werden heruntergeladen....")
+    print("Gebäudereferenzen werden heruntergeladen....")
     r = requests.get(url, allow_redirects=True)
     open('gebref.zip', 'wb').write(r.content)
     print('Gebäudereferenzen werden entpackt...')
     shutil.unpack_archive("gebref.zip")
-    print ("Gebäudereferenzen wurden entpackt")
-    
+    print("Gebäudereferenzen wurden entpackt!")
 
-def StrassentabelleAusgeben(Kreis,Gemeindename):
+
+def StrassentabelleAusgeben(Kreis, Gemeindename):
     curgem = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     SQL = "select * from gebref where gmd='{}'".format(Gemeindename)
     print(SQL)
@@ -45,12 +43,15 @@ def StrassentabelleAusgeben(Kreis,Gemeindename):
     for row in curgem:
         row_count += 1
         print(row_count)
-        zeile1 = row['landschl'] + ";" + row['regbezschl'] + ";" + row['kreisschl'] + ";" + row['gmdschl'] + ";"
+        zeile1 = row['landschl'] + ";" + row['regbezschl'] + \
+            ";" + row['kreisschl'] + ";" + row['gmdschl'] + ";"
 
-    F2 = open("./output/" + slugify.slugify(Kreis) + "_" + slugify.slugify(Gemeindename) + "_Strassen.txt", "w", encoding="windows-1252", newline='\r\n')
-    #F2 = open("./output/" + slugify.slugify(Kreis) + "_" + slugify.slugify(Gemeindename) + "_Strassen.txt", "w")
+    F2 = open("./output/" + slugify.slugify(Kreis) + "_" + slugify.slugify(Gemeindename) +
+              "_Strassen.txt", "w", encoding="windows-1252", newline='\r\n')
+    # F2 = open("./output/" + slugify.slugify(Kreis) + "_" + slugify.slugify(Gemeindename) + "_Strassen.txt", "w")
     curstrasse = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    SQL2 = "select distinct strschl, str from gebref where gmd='" + Gemeindename + "' order by str"
+    SQL2 = "select distinct strschl, str from gebref where gmd='" + \
+        Gemeindename + "' order by str"
     curstrasse.execute(SQL2)
     # print(SQL2)
     records = curstrasse.fetchall()
@@ -61,7 +62,7 @@ def StrassentabelleAusgeben(Kreis,Gemeindename):
     F2.close()
 
 
-def Hausnummerntabelle(Kreis,Gemeindename):
+def Hausnummerntabelle(Kreis, Gemeindename):
     curgem = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     SQL = "select * from gebref where gmd='{}'".format(Gemeindename)
     print(SQL)
@@ -72,9 +73,11 @@ def Hausnummerntabelle(Kreis,Gemeindename):
     for row in curgem:
         row_count += 1
         print(row_count)
-        zeile1 = row['landschl'] + ";" + row['regbezschl'] + ";" + row['kreisschl'] + ";" + row['gmdschl'] + ";"
+        zeile1 = row['landschl'] + ";" + row['regbezschl'] + \
+            ";" + row['kreisschl'] + ";" + row['gmdschl'] + ";"
 
-    F2 = open("./output/"  + slugify.slugify(Kreis) + "_" + slugify.slugify(Gemeindename) + "_Hausnummern.txt", "w", encoding="windows-1252", newline='\r\n')
+    F2 = open("./output/" + slugify.slugify(Kreis) + "_" + slugify.slugify(Gemeindename) +
+              "_Hausnummern.txt", "w", encoding="windows-1252", newline='\r\n')
     curstrasse = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     SQL2 = "select strschl, hnr, adz, st_x(geom31466)::text as x, st_y(geom31466)::text as y  from " \
            "gebref where gmd='" + Gemeindename + "' order by str,hnr"
@@ -83,116 +86,18 @@ def Hausnummerntabelle(Kreis,Gemeindename):
     records = curstrasse.fetchall()
     for strasse in records:
         zeile = strasse["strschl"] + ";" + strasse["hnr"] + ";" + strasse["adz"] \
-                + ";001;" + strasse["x"] + ";" + strasse["y"] + ";A;00\n"
+            + ";001;" + strasse["x"] + ";" + strasse["y"] + ";A;00\n"
         zeile2 = str(zeile1) + str(zeile)
         print(zeile2)
         F2.write(zeile2)
     F2.close()
-
-
-
 
 
 def truncateGebref():
+    """Gebäudereferenztabellen leeren
+    """
     cur.execute("truncate gebref_schluessel")
     cur.execute("truncate gebref")
-
-
-def truncateKreis():
-    cur.execute("truncate gmadressen")
-
-
-def StrassentabelleAusgebenAlt(Gemeindename):
-    
-    #
-    #update gebref set geom31466=st_transform(geom25832,'EPSG:25832','EPSG:31466');
-
-    #
-    curgem = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    SQL = "select * from gebref_schluessel where field_6='{}'".format(Gemeindename)
-    print(SQL)
-    curgem.execute(SQL)
-    print("Nach Execute")
-    row_count = 0
-    zeile1 = ""
-    for row in curgem:
-        row_count += 1
-        #   print(row_count)
-        zeile1 = row['field_2'] + ";" + row['field_3'] + ";" + row['field_4'] + ";" + row['field_5'] + ";"
-
-    F2 = open(Gemeindename + "_Strassen.txt", "w", encoding="windows-1252", newline='\r\n')
-    curstrasse = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    SQL2 = "select distinct strassenschluessel, strassenname from gmadressen where gemeinde_stadt='" + Gemeindename + "' order by strassenname"
-    curstrasse.execute(SQL2)
-    # print(SQL2)
-    records = curstrasse.fetchall()
-    for strasse in records:
-        zeile = strasse["strassenschluessel"] + ";0;" + strasse["strassenname"] + "\n"
-        zeile2 = str(zeile1) + str(zeile)
-        F2.write(zeile2)
-    F2.close()
-
-
-def HausnummerntabelleAlt(Gemeindename):
-    curgem = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    SQL = "select * from gebref_schluessel where field_6='{}'".format(Gemeindename)
-    print(SQL)
-    curgem.execute(SQL)
-    print("Nach Execute")
-    row_count = 0
-    zeile1 = ""
-    for row in curgem:
-        row_count += 1
-        print(row_count)
-        zeile1 = row['field_2'] + ";" + row['field_3'] + ";" + row['field_4'] + ";" + row['field_5'] + ";"
-
-    F2 = open(Gemeindename + "_Hausnummern.txt", "w", encoding="windows-1252")
-    curstrasse = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    SQL2 = "select strassenschluessel, nummernteil, buchstaben, st_x(geom31466)::text as x, st_y(geom31466)::text as y  from " \
-           "gmadressen where gemeinde_stadt='" + Gemeindename + "' order by strassenname, hsnr"
-    curstrasse.execute(SQL2)
-    print(SQL2)
-    records = curstrasse.fetchall()
-    for strasse in records:
-        zeile = strasse["strassenschluessel"] + ";" + strasse["nummernteil"] + ";" + strasse["buchstaben"] \
-                + ";001;" + strasse["x"] + ";" + strasse["y"] + ";A;00\n"
-        zeile2 = str(zeile1) + str(zeile)
-        print(zeile2)
-        F2.write(zeile2)
-    F2.close()
-
-
-def kreisdaten_einlesen():
-    print ("Kreidaten einlesen aufgerufen")
-    # Aktuelle Daten der Kreisverwaltung einlesen
-    cur.execute("truncate gmadressen")
-    print ("Truncate durchgeführt")
-    F = open("hausnummern_polizei_.csv", "r", encoding="windows-1252")
-    print ("Datei geöffnet")
-    i = -1;
-
-    for line in F:
-        print("Zeile gelesen")
-        line = line.strip()
-        print(line + "     " + str(line.count(";")))
-        m = re.split(';', line)
-        print(m[0] + "  " + str(m[5]))
-        SQL = "insert into gmadressen (gemeinde_stadt, ortsteil, strassenschluessel, strassenname, hsnr, ostwert, nordwert) " \
-              "values (%s, %s, %s, %s, %s, %s, %s);"
-        data = (m[0], m[1], m[2], m[3], m[4], m[5], m[6])
-        if i >= 0:
-            cur.execute(SQL, data)
-        i += 1
-    conn.commit()
-    SQL = """update gmadressen set 
-    geom25832=st_setsrid(st_point(cast(replace(ostwert,',','.') as float), cast(replace(nordwert,',','.') as float)),25832)"""
-    cur.execute(SQL)
-    conn.commit()
-    SQL = "update gmadressen set geom31466=st_transform(geom25832,31466)"
-    cur.execute(SQL)
-    SQL = "update gmadressen set nummernteil=substring(hsnr,'^[\\d]*'), buchstaben=substring(hsnr,'[a-z]*$')"
-    cur.execute(SQL)
-    conn.commit()
 
 
 def gemeindeschluessel_einlesen():
@@ -212,8 +117,8 @@ def gemeindeschluessel_einlesen():
     conn.commit()
 
 
-def gebaeudereferenzen_einlesen():
-    print ("Lese Gebäudereferenzen ein. Dies kann etwas dauern ......")
+def gebaeudereferenzen_einlesen(nurOberbergLaden):
+    print("Lese Gebäudereferenzen ein. Dies kann etwas dauern ......")
     # Aktuelle Gebäudereferenzen des Landes einlesen
     F = open("gebref.txt", "r", encoding="utf-8")
     i = -1
@@ -221,46 +126,36 @@ def gebaeudereferenzen_einlesen():
     for line in F:
         line = line.strip()
         m = re.split(';', line)
-        #print ('Spalten:' + str(len(m)))
-        punkt = "POINT(" + m[18].replace(",", ".") + " " + m[19].replace(",", ".") + ")"
+        # print ('Spalten:' + str(len(m)))
+        punkt = "POINT(" + m[18].replace(",", ".") + \
+            " " + m[19].replace(",", ".") + ")"
         SQL = """INSERT INTO public.gebref(nba, oid, qua, landschl, land, regbezschl, regbez, kreisschl, kreis, gmdschl, gmd, ottschl, ott, strschl, str, hnr, adz, zone, ostwert, nordwert, datum, geom25832)
                 VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-        data = (m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11], m[12], m[13], m[14],m[15],m[16],m[17],m[18],m[19],m[20],punkt) 
-        #if m[3]=="05" and m[5]=='3' and m[7]=='74':
-        if i >= 0:
-            cur.execute(SQL, data)
-        if i == 1000:
-            conn.commit()
-            i = 0
-            print("\r" + m[1])
-        i += 1
+        data = (m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10],
+                m[11], m[12], m[13], m[14], m[15], m[16], m[17], m[18], m[19], m[20], punkt)
+        if (nurOberbergLaden):
+            if m[3] == "05" and m[5] == '3' and m[7] == '74':
+                if i >= 0:
+                    cur.execute(SQL, data)
+                if i == 1000:
+                    conn.commit()
+                    i = 0
+                    print("\r" + m[1])
+                i += 1
+        else:
+            if i >= 0:
+                cur.execute(SQL, data)
+            if i == 1000:
+                conn.commit()
+                i = 0
+                print("\r" + m[1])
+            i += 1
 
-    print ('Geom31466 erstellen')
-    SQL='update gebref set geom31466=st_transform(geom25832,31466)'
+    print('Geom31466 erstellen')
+    SQL = 'update gebref set geom31466=st_transform(geom25832,31466)'
     cur.execute(SQL)
     conn.commit()
-    
-    # print("\rBaue Straßennamen zusammen")
-    # SQL = """update gebref set adresse = strassenname || ' ' || hnr || coalesce(zusatz,'')"""
-    # cur.execute(SQL)
-    # conn.commit()
-    # print("\rFülle Geospalten")
-    # SQL = """update gebref set geom4647=st_setsrid(st_point(cast(replace(rechts,',','.') as float), cast(replace(hoch,',','.') as float)),4647),
-    # geom25832=st_setsrid(st_point(cast(replace(rechts,',','.') as float), cast(replace(hoch,',','.') as float)),25832),
-    # geom31466=st_setsrid(st_point(cast(replace(rechts,',','.') as float), cast(replace(hoch,',','.') as float)),31466)"""
-    # cur.execute(SQL)
-    # conn.commit()
-    # print("\rsetze Gemeindenamen")
-    # SQL = """update gebref set gemeindename=(select field_6 from gebref_schluessel a where a.field_3=regbez and a.field_4=kreis and a.field_5=gemeinde) where gemeinde<>'000';"""
-    # cur.execute(SQL)
-    # conn.commit()
-    # SQL = """update gebref set gemeindename=(select field_5 from gebref_schluessel a where a.field_3=regbez and a.field_4=kreis and a.field_1='K') where gemeinde='000';"""
-    # cur.execute(SQL)
-    # conn.commit()
     print("\rFertig!")
-
-
-
 
 
 class DatenDialog(QtWidgets.QMainWindow):
@@ -276,26 +171,20 @@ class DatenDialog(QtWidgets.QMainWindow):
         exit(1)
 
     def weiter(self):
-        ausfuehren(self.ui.truncateGebref.isChecked(),self.ui.truncateKreis.isChecked(),self.ui.importGebref.isChecked(),self.ui.importKreis.isChecked(),self.ui.exportCebius.isChecked(),self.ui.CheckboxGebrefHolen.isChecked(),self.ui.UrlGebrefHolen.text())
+        ausfuehren(self.ui.truncateGebref.isChecked(),  self.ui.importGebref.isChecked(
+        ), self.ui.exportCebius.isChecked(), self.ui.CheckboxGebrefHolen.isChecked(), self.ui.UrlGebrefHolen.text(), self.ui.checkBoxNurOberbergLaden.isChecked())
         exit(0)
 
 
-def ausfuehren(truncGebref,truncKreis,importGebref,importKreis,exportCebius,gebrefHolen,gebrefUrl):
-    print ("ausführen wurde aufgerufen!")
-    
+def ausfuehren(truncGebref,  importGebref,  exportCebius, gebrefHolen, gebrefUrl, checkBoxNurOberbergLaden):
+    print("ausführen wurde aufgerufen!")
+
     if (gebrefHolen):
         GebrefLadenUndEntpacken(gebrefUrl)
 
     if (importGebref):
-        #gemeindeschluessel_einlesen()
-        gebaeudereferenzen_einlesen()
-
-    if (importKreis):
-        kreisdaten_einlesen()
-
-    # gemeinden = (
-    # "Radevormwald", "Hückeswagen", "Wipperfürth", "Lindlar", "Gummersbach", "Bergneustadt", "Wiehl", "Reichshof",
-    # "Nümbrecht", "Engelskirchen", "Waldbröl", "Morsbach", "Marienheide")
+        # gemeindeschluessel_einlesen()
+        gebaeudereferenzen_einlesen(checkBoxNurOberbergLaden)
 
     # Gemeinden aus Datenbank auslesen
     SQL = "select distinct kreis, gmd from gebref order by kreis, gmd"
@@ -305,15 +194,20 @@ def ausfuehren(truncGebref,truncKreis,importGebref,importKreis,exportCebius,gebr
     gemeinden = curgemeinde.fetchall()
     print(gemeinden)
     for gemeinde in gemeinden:
-        print (gemeinde[0], gemeinde[1])
-
+        print(gemeinde[0], gemeinde[1])
     if (exportCebius):
+#        GemeindenamenUndKennungenAusgeben()
         for gemeinde in gemeinden:
-            print(gemeinde)
-            StrassentabelleAusgeben(gemeinde[0],gemeinde [1])
-            Hausnummerntabelle(gemeinde[0], gemeinde [1])
+            print("Tabellen ausgeben für: " + str(gemeinde))
+            StrassentabelleAusgeben(gemeinde[0], gemeinde[1])
+            Hausnummerntabelle(gemeinde[0], gemeinde[1])
     cur.close()
     conn.close()
+    exit()
+
+
+def GemeindenamenUndKennungenAusgeben():
+    exit()
 
 
 def main():
@@ -325,4 +219,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
